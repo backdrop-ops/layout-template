c3ba4343912357d5cb5d416242fbbe02534d612a
Peter Anderson <peter@panda.id.au>
2019-11-19 20:55:54 +0200

    Initial commit.

---
+++ my_layout.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * PHP functions for the My Layout layout.
+ */
+
+/**
+ * Process variables for the My Layout layout.
+ */
+function template_preprocess_layout__my_layout(&$variables) {
+  $variables['my_layout'] = 'is-awesome';
+}
